8cc735661e7e87dbc6d194092f70d7e963c446e3
Andy Hazard <andyhazard@macbook-air-de-andy.home>
2021-04-04 20:24:04 +0200

    big commit: i can extract data end show names from the jsonholder file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
-
+import { JSONPlaceholderService } from './services/jsonplaceholder.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'cyberlead-project';
+
+    
+  data:Array<any>
+
+  constructor(private JSONplaceholder: JSONPlaceholderService) {
+    this.data = new Array<any>()
+  };
+
+  ngOnInit(){
+    this.JSONplaceholder.getData().subscribe((data) => {
+      console.log(data)
+      this.data = data
+    })
+  };
+
 }
+
+
+  
